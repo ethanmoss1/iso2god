b1d9888e589c3787a80a604e42bd9521ec963bfe
Ethan Moss <cywinskimoss@gmail.com>
2025-10-02 20:51:41 +0100

    Added build inputs and removed devShell

--- flake.nix
+++ flake.nix
@@ -8,18 +8,18 @@
       system = "x86_64-linux"; # As this is for T2 Macs, only x86_64 needed.
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShell = pkgs.mkShell {
-        buildInputs=[
-          pkgs.pkg-config
-          pkgs.rust-openssl
-        ];
-      };
-
       packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
         pname = "iso2god";
         version = "0.1.0";
         src = ./.;
         cargoHash = "sha256-1q2ruR2FFtIjBBR4E9Z/icbeVaec2QzWWXbHouJ2+do=";
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+        buildInputs = [
+          pkgs.openssl
+        ];
       };
     };
 }
